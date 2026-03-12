b28b6cf61faa332684f68f4f9e8d62c4608de07a
Tiara <azzahratiaraputri@gmail.com>
2026-03-12 08:26:52 +0700

    push pertama

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <View style={styles.row}>
+        <View style={styles.box1}></View>
+        <View style={styles.box2}></View>
+        <View style={styles.box3}></View>
+        <View style={styles.box4}></View>
+      </View>
+      <Text style={styles.title}>Balok Warna</Text>
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+  
+  title: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 20
   },
+
+
+  container: {
+    height: 250,
+    backgroundColor: '#f186e5',
+    justifyContent: 'flex-start',
+    paddingTop: 50
+  },
+
+  row: {
+    flexDirection: 'row'
+  },
+
+   box1: {
+    width: 100,
+    height: 40,
+    backgroundColor: 'red'
+  },
+
+  box2: {
+    width: 100,
+    height: 70,
+    backgroundColor: 'blue'
+  },
+
+  box3: {
+    width: 100,
+    height: 110,
+    backgroundColor: 'green'
+  },
+
+  box4: {
+    width: 100,
+    height: 150,
+    backgroundColor: 'orange'
+  }
+
 });
